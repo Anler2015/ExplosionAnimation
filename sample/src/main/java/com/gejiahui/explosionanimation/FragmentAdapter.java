add965350c1357319affeb593af7835d07b80263
葛佳辉 <hippogjh7758258@qq.com>
2016-01-05 23:30:36 +0800

    修改sample为Fragment展示方式

---
+++ sample/src/main/java/com/gejiahui/explosionanimation/FragmentAdapter.java
@@ -0,0 +1,29 @@
+package com.gejiahui.explosionanimation;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by gejiahui on 2016/1/5.
+ */
+public class FragmentAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> list = new ArrayList<Fragment>();
+
+    public FragmentAdapter(FragmentManager fm, ArrayList<Fragment> list) {
+        super(fm);
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+}
--- sample/src/main/java/com/gejiahui/explosionanimation/MainActivity.java
+++ sample/src/main/java/com/gejiahui/explosionanimation/MainActivity.java
@@ -2,41 +2,37 @@ package com.gejiahui.explosionanimation;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
 import com.gejiahui.library.explosionanimation.ExplosionField;
-import com.gejiahui.library.explosionanimation.particle.ExplodeParticle;
 import com.gejiahui.library.explosionanimation.particle.FlyAwayParticle;
-import com.gejiahui.library.explosionanimation.particle.LeftParticle;
-import com.gejiahui.library.explosionanimation.particle.RightParticle;
+
+
+import java.util.ArrayList;
 
 /**
  * Created by gejiahui on 2015/12/28.
  */
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
     private ExplosionField mExplosionField;
-
-
+    private ViewPager viewPager;
+    private ArrayList<Fragment> list = new ArrayList<Fragment>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         mExplosionField = ExplosionField.attach2Window(this);
-   //     addListener(findViewById(R.id.root));
-
-        ImageView img1 = (ImageView)findViewById(R.id.img1);
-        ImageView img2 = (ImageView)findViewById(R.id.img2);
-        ImageView img3 = (ImageView)findViewById(R.id.img3);
-
-//        img1.setOnClickListener(this);
-//        img2.setOnClickListener(this);
-//        img3.setOnClickListener(this);
+        viewPager = (ViewPager) findViewById(R.id.view_page);
 
 
+        ExplodeViewFragment explodeViewFragment = new ExplodeViewFragment();
+        list.add(explodeViewFragment);
+        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager(),list);
+        viewPager.setAdapter(adapter);
 
     }
 
@@ -59,24 +55,11 @@ public class MainActivity extends Activity implements View.OnClickListener{
     }
 
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-      mExplosionField.explode(event, new LeftParticle());
-        return true;
-    }
+//    @Override
+//    public boolean onTouchEvent(MotionEvent event) {
+//      mExplosionField.explode(event, new LeftParticle());
+//        return true;
+//    }
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()){
-            case R.id.img1:
-                mExplosionField.explode(v, new RightParticle());
-                break;
-            case R.id.img2:
-                mExplosionField.explode(v, new FlyAwayParticle());
-                break;
-            case R.id.img3:
-                mExplosionField.explode(v, new ExplodeParticle());
-                break;
-        }
-    }
+
 }
