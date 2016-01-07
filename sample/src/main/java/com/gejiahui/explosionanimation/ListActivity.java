01bdbc8980d5085c090bada802d52eaeb6323371
葛佳辉 <hippogjh7758258@qq.com>
2016-01-08 00:10:41 +0800

    修改list
    因viewpage会产生滑动冲突
    改成activity形式

---
+++ sample/src/main/java/com/gejiahui/explosionanimation/ListActivity.java
@@ -0,0 +1,94 @@
+package com.gejiahui.explosionanimation;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.gejiahui.library.explosionanimation.ExplosionField;
+import com.gejiahui.library.explosionanimation.particle.LeftParticle;
+import com.gejiahui.library.explosionanimation.particle.RightParticle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by gejiahui on 2016/1/6.
+ */
+public class ListActivity extends Activity {
+    private ListView list;
+    private List<String> mDatas;
+    private ExplosionField mExplosionField;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list);
+        list = (ListView)findViewById(R.id.list);
+        mExplosionField = ExplosionField.attach2Window(this);
+        initData();
+        MyAdapter adapter = new MyAdapter();
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+
+
+    private void initData(){
+        mDatas = new ArrayList<String>();
+        for(int i = 0 ; i < 20 ; i++){
+
+            mDatas.add("This is "+ i +" item ");
+        }
+    }
+
+    class MyAdapter extends BaseAdapter {
+
+        public MyAdapter() {
+        }
+
+        @Override
+        public int getCount() {
+            return mDatas.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDatas.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+            if(convertView != null){
+                view = convertView;
+            }else{
+                view = LayoutInflater.from(ListActivity.this).inflate(R.layout.item,parent,false);
+            }
+            TextView txt = (TextView)view.findViewById(R.id.txt);
+            txt.setText(mDatas.get(position));
+            return view;
+        }
+    }
+
+
+}
--- sample/src/main/java/com/gejiahui/explosionanimation/MainActivity.java
+++ sample/src/main/java/com/gejiahui/explosionanimation/MainActivity.java
@@ -1,14 +1,22 @@
 package com.gejiahui.explosionanimation;
 
-import android.app.Activity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import com.gejiahui.library.explosionanimation.ExplosionField;
+import com.gejiahui.library.explosionanimation.particle.ExplodeParticle;
 import com.gejiahui.library.explosionanimation.particle.FlyAwayParticle;
+import com.gejiahui.library.explosionanimation.particle.RightParticle;
 
 
 import java.util.ArrayList;
@@ -19,22 +27,22 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ExplosionField mExplosionField;
-    private ViewPager viewPager;
+
     private ArrayList<Fragment> list = new ArrayList<Fragment>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         mExplosionField = ExplosionField.attach2Window(this);
-        viewPager = (ViewPager) findViewById(R.id.view_page);
+        ImageView img1 = (ImageView) findViewById(R.id.img1);
+        ImageView img2 = (ImageView) findViewById(R.id.img2);
+        ImageView img3 = (ImageView) findViewById(R.id.img3);
+        mExplosionField = ExplosionField.attach2Window(this);
+        img1.setOnClickListener(clickListener);
+        img2.setOnClickListener(clickListener);
+        img3.setOnClickListener(clickListener);
 
-
-        ExplodeViewFragment explodeViewFragment = new ExplodeViewFragment();
-        ListFragment listFragment = new ListFragment();
-        list.add(explodeViewFragment);
-        list.add(listFragment);
-        FragmentAdapter adapter = new FragmentAdapter(getSupportFragmentManager(),list);
-        viewPager.setAdapter(adapter);
 
     }
 
@@ -57,11 +65,56 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//      mExplosionField.explode(event, new LeftParticle());
-//        return true;
-//    }
+
+    View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(final View v) {
+            switch (v.getId()) {
+                case R.id.img1:
+                    mExplosionField.explode(v, new RightParticle());
+                    break;
+                case R.id.img2:
+                    mExplosionField.explode(v, new FlyAwayParticle());
+                    break;
+                case R.id.img3:
+                    mExplosionField.explode(v, new ExplodeParticle());
+                    break;
+            }
+            ValueAnimator valueAnimator = new ValueAnimator().ofFloat(1).setDuration(1700);
+            valueAnimator.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    v.animate().alpha(1).scaleY(1).scaleX(1).setDuration(150).start();
+                }
+            });
+            valueAnimator.start();
+        }
 
 
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(MainActivity.this,ListActivity.class);
+            startActivity(intent);
+
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
